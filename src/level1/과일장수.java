a72e4468cf98ea7080b68f819a0158ba45631400
Kim Seong Ho <74396651+OOOIOOOIO@users.noreply.github.com>
2023-01-25 18:18:59 +0900

    Create 과일장수.java

---
+++ src/level1/과일장수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+static class Solution {
+        public int solution(int k, int m, int[] score) {
+            int answer = 0;
+            
+            Integer[] temp = Arrays.stream(score)
+                    .boxed()
+                    .toArray(Integer[]::new);
+
+            Arrays.sort(temp, (a, b) -> b-a);
+
+            int boxNum = temp.length / m;
+
+            Integer[][] box = new Integer[boxNum][m];
+
+
+
+            int n = 0;
+            for(int i = 0; i < boxNum; i++){
+                for(int j = 0; j < m; j++){
+                    box[i][j] = temp[n];
+                    n++;
+                }
+            }
+
+
+
+            for(int i = 0; i < boxNum; i++){
+                    answer += (box[i][m-1] * m);
+            }
+
+            return answer;
+        }
+    }
