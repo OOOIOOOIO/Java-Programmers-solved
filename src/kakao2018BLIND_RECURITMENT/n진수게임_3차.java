1c13d09dbafda849ea2dcf52a1c1dee4ab596481
OOOIOOOIO <polite159@gmail.com>
2023-07-11 12:23:23 +0900

    Integer.toString(i, n) -> i를 n진수로 변환. sb에 append하면서 length()를 제한 조건으로... 똑똑하다

---
+++ src/kakao2018BLIND_RECURITMENT/n진수게임_3차.java
@@ -0,0 +1,33 @@
+package kakao2018BLIND_RECURITMENT;
+
+public class n진수게임_3차 {
+
+    public String solution(int n, int t, int m, int p) {
+
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        // 진수 변환해서 붙이기
+        for(int i = 0; sb.length() < m * t; i++){
+            sb.append(Integer.toString(i, n));
+        }
+
+        // 내가 할 것만 고르기, 0 부터 시작
+        for(int i = p-1; sb2.length() < t; i += m){
+            sb2.append(sb.toString().charAt(i));
+        }
+
+        String answer = sb2.toString().toUpperCase();
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        String s = Integer.toString(11, 16);
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("args = " + s);
+
+        sb.
+    }
+}
