2d17fe2e03571295538def407c6369ae0f41e427
OOOIOOOIO <polite159@gmail.com>
2023-10-09 13:51:33 +0900

    method 분리 및 쉽게 구현하기

--- src/level2/문자열압축.java
+++ src/level2/문자열압축.java
@@ -1,13 +1,72 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 문자열압축 {
 	public static void main(String[] args) {
 
-		int a = solution("abcabcdede");
+		int a = solve1("abcabcdede");
+		int b = solve2("abcabcdede");
 		System.out.println(a);
 		
 	}
-    public static  int solution(String s) {
+
+	public static int solve2(String s){
+		int min = Integer.MAX_VALUE;
+		// 문자열 자르기 순회;
+		for(int i = 1; i <= s.length(); i++){
+			// 문자열 압축
+			int result = compress(s, i);
+
+			min = Math.min(result, min);
+
+
+
+		}
+
+		return min;
+	}
+
+	private static List<String> split(String s, int length){
+		List<String> token = new ArrayList<>();
+		int endIndex = 0;
+		for(int startIndex = 0; startIndex < s.length(); startIndex += length){
+			if(startIndex + length > s.length()) endIndex = s.length();
+			else endIndex = startIndex + length;
+
+			String str = s.substring(startIndex, endIndex);
+			token.add(str);
+		}
+
+		return token;
+
+	}
+
+	private static int compress(String s, int length){
+		StringBuilder sb = new StringBuilder();
+		String last = "";
+		int cnt = 1;
+
+		for(String token : split(s, length)){
+			if(token.equals(last)){
+				cnt++;
+			}
+			else{
+				if(cnt > 1) sb.append(cnt);
+				sb.append(last);
+				cnt = 1;
+				last = token;
+			}
+
+		}
+		// 맨 마지막 추가
+		if(cnt > 1) sb.append(cnt);
+		sb.append(last);
+
+		return sb.length();
+	}
+    public static  int solve1(String s) {
     	int answer = s.length();
     	
     	for(int i = 1; i <= s.length() / 2; i++) {
