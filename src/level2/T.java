f959052e0e615ecad08da1b44e470fa6d179ea2d
OOOIOOOIO <polite159@gmail.com>
2023-10-09 13:51:54 +0900

    추가 commit

--- src/level2/T.java
+++ src/level2/T.java
@@ -7,7 +7,6 @@ public class T {
 
         StringBuilder sb = new StringBuilder();
         sb.reverse().toString();
-        sb.append
         System.out.println('z' - 'a');
         System.out.println('a'-'0');
         System.out.println('z'-'0');
