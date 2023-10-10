c75df9a218efe880364b25efccd7bb0a03a18408
OOOIOOOIO <polite159@gmail.com>
2023-10-10 17:46:03 +0900

    String.replace : 전체 다 바꾸는 것, 헷갈렸네

--- src/level1/Test.java
+++ src/level1/Test.java
@@ -6,66 +6,16 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Test {
-	
-	public static void main(String[] args) throws IOException{
-		
 
-		
-	}
-	
-    public static int[] solution(int[] answers) {
-        
-        
-        int length = answers.length;
-        
-        int[] s1 = {1, 2, 3, 4, 5}; // 5
-        int[] s2 = {2, 1, 2, 3, 2, 4, 2, 5}; // 8
-        int[] s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
-        
-        int k1 = 0;
-        int k2 = 0;
-        int k3 = 0;
-        
-        int[] cnt = {0, 0, 0};
-        
-        for(int i = 0; i < length; i++) {
-        	
-        	if(k1 == 5) k1 = 0;
-        	if(k2 == 8) k2 = 0;
-        	if(k3 == 10) k3 = 0;
+    public static void main(String[] args) throws IOException {
+        String s = "pypypyp";
 
-        	
-        	if(answers[i] == s1[k1]) cnt[0]++;        		
-        	if(answers[i] == s2[k2]) cnt[1]++;        		
-        	if(answers[i] == s3[k3]) cnt[2]++;        		
-        	
-        	
-        	
-        	k1++;
-        	k2++;
-        	k3++;
-        	
-//        	이게 더 보기 깔끔하다
-//	    	if(s1[i % s1.length] == answers[i]) cnt[0]++;
-//	        if(s2[i % s2.length] == answers[i]) cnt[1]++;
-//	        if(s3[i % s3.length] == answers[i]) cnt[2]++;
-        }
-        
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        
-        int max = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
-        
-        if(max == cnt[0]) list.add(1);
-        if(max == cnt[1]) list.add(2);
-        if(max == cnt[2]) list.add(3);
-        
-        int[] answer = new int[list.size()];
-        
-        for(int i = 0; i < answer.length; i++) {
-        	answer[i] = list.get(i);
-        }
-        
-        
-        return answer;
-    }	
+        String p = s.replace("p", "");
+        String p1 = s.replace('p', 'a');
+        String pp = s.replaceAll("p", "a");
+        System.out.println(p);
+        System.out.println(p1);
+        System.out.println(pp);
+
+    }
 }
--- src/level1/문자열내p와y의개수.java
+++ src/level1/문자열내p와y의개수.java
@@ -8,6 +8,15 @@ public class 문자열내p와y의개수 {
 	public static void main(String[] args) {
 		
 	}
+
+	static boolean solve2(String str){
+		String s = str.toLowerCase();
+
+		int p = s.replace("p", "").length();
+		int y = s.replace("y", "").length();
+
+		return p == y;
+	}
 	
     static boolean solution(String s) {
         boolean answer = false;
