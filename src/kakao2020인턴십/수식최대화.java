abb71f90a0012078a8f97015001251cb2aa16165
OOOIOOOIO <polite159@gmail.com>
2023-10-16 19:47:50 +0900

    메서드 분할 연습

--- src/kakao2020인턴십/수식최대화.java
+++ src/kakao2020인턴십/수식최대화.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class 수식최대화 {
 
+
     static class Solution{
         static ArrayList<String> expList = new ArrayList<String>();
         static String[][] priority = {
@@ -16,6 +17,77 @@ public class 수식최대화 {
 
 
         public static long solution(String expression){
+
+            return solve1(expression);
+        }
+
+
+
+
+
+
+
+
+
+        public static long solve1(String expression){
+            ArrayList<String> tokens = new ArrayList<String>();
+            StringTokenizer st = new StringTokenizer(expression, "+-*", true);
+
+            // 추가
+            while(st.hasMoreTokens()){
+                tokens.add(st.nextToken());
+            }
+            long max = 0;
+            //계산
+            for(String[] precedence : priority){
+                long result = Math.abs(calculate(new ArrayList<>(tokens), precedence));
+                if(result > max) max = result;
+
+            }
+
+            return max;
+        }
+
+        private static long calculate(List<String> tokens, String[] exp){
+            for(String op : exp){
+                for(int i = 0; i < tokens.size(); i++){
+                    if(tokens.get(i).equals(op)){
+                        long lhs = Long.parseLong(tokens.get(i-1));
+                        long rhs =Long.parseLong(tokens.get(i+1));
+                        long calculate = calculate(lhs, rhs, op);
+                        tokens.remove(i-1);
+                        tokens.remove(i-1);
+                        tokens.remove(i-1);
+                        tokens.add(i - 1, String.valueOf(calculate));
+                        i -= 2;
+
+                    }
+                }
+
+            }
+
+            return Long.parseLong(tokens.get(0));
+        }
+
+        private static long calculate(Long lhs, Long rhs, String op){
+            long result = 0;
+
+            switch (op){
+                case "+" : result = lhs + rhs; break;
+                case "-" : result = lhs - rhs; break;
+                case "*" : result = lhs * rhs; break;
+                default: result = 0;
+            };
+
+            return result;
+
+        }
+
+
+
+
+
+        public static long solve2(String expression){
             System.out.println(priority[0].length);
             System.out.println(priority.length);
             long answer = 0;
