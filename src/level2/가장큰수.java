8c2bb60d772d633ca634a00723514e9b923a2025
OOOIOOOIO <polite159@gmail.com>
2023-10-23 10:45:18 +0900

    문자열 비교, 문자열을 붙여서 정수형으로 비교할 때 다른데, 흠...

--- src/level2/가장큰수.java
+++ src/level2/가장큰수.java
@@ -1,6 +1,7 @@
 package level2;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class 가장큰수 {
 
@@ -16,11 +17,16 @@ public class 가장큰수 {
                     int reversed = Integer.parseInt(s2 + s1);
                     System.out.println("reversed = " + reversed);
                     System.out.println();
-                    return original - reversed;
+                    return s1.compareTo(s2);
+//                    return original - reversed;
 //                    return reversed - original; // s1이 s2 보다 더 크다는 뜻... s1이 더 뒤로 정렬된다(내림차순)
 
                 })
                 .forEach(System.out::println);
+//                .collect(Collectors.joining())
+//                .replaceAll("^0+", "0"); // TC 중 0으로만 이루어진 것들이 있음
+
+
     }
 }
 
@@ -50,4 +56,34 @@ public class 가장큰수 {
  * 9
  * 99
  * 9999
+ *
+ *
+ * =========
+ *
+ * return s2.compareTo(s1)일 경우
+ *
+ * 9999
+ * 99
+ * 9
+ * 555
+ * 5
+ * 34
+ * 30000
+ * 30
+ * 3
+ *
+ * =========
+ *
+ * return s1.compareTo(s2)일 경우
+ *
+ * 3
+ * 30
+ * 30000
+ * 34
+ * 5
+ * 555
+ * 9
+ * 99
+ * 9999
+ *
  */
