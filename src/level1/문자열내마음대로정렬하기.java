2a9508487984e2defc67cee0259fb45bc0321584
OOOIOOOIO <polite159@gmail.com>
2023-10-23 10:37:51 +0900

    steam 사용

--- src/level1/문자열내마음대로정렬하기.java
+++ src/level1/문자열내마음대로정렬하기.java
@@ -15,6 +15,25 @@ public class 문자열내마음대로정렬하기 {
 		}
 		
 	}
+
+	public static String[] solve2(String[] strings, int n) {
+
+		//1
+		Arrays.sort(strings, (s1, s2) -> {
+			if(s1.charAt(n) - s2.charAt(n) < 0) return -1;
+			else if(s1.charAt(n) - s2.charAt(n) > 0) return 1;
+			else return s1.compareTo(s2);
+		});
+
+		Arrays.sort(strings, (s1, s2) -> {
+			if(s1.charAt(n) != s2.charAt(n)) return s1.charAt(n) - s2.charAt(n);
+			else return s1.compareTo(s2);
+		});
+
+		return strings;
+
+
+	}
 	
 	public static String[] solution(String[] strings, int n) {
 		String[] answer = new String[strings.length];
